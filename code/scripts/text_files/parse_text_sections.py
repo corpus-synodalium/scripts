bdb5ff7baabca809106bb606fcbcbf42614afb71
thawsitt <thawsitt@cs.stanford.edu>
2018-04-04 12:37:12 -0700

    Fixed bugs in parse_text_sections.py. See github comment for details.

--- code/scripts/text_files/parse_text_sections.py
+++ code/scripts/text_files/parse_text_sections.py
@@ -16,8 +16,8 @@ def main():
     counter = 0
     for f in input_file_names:
         lines = readFile(f)
-        dictObj = readIntoDict(lines)
-        createJsonFile(dictObj, f)
+        dictObj = readIntoDict(lines, f)
+        createOutputFile(dictObj, f)
         counter += 1
 
     print('Number of input txt files: {}'.format(len(input_file_names)))
@@ -27,35 +27,19 @@ def main():
 # Helper Functions
 # ----------------
 
-def createJsonFile(dictObj, filename):
+def createOutputFile(dictObj, filename):
     json_file_name = './output/{}_output.txt'.format(re.findall(r'^(.*?)\.txt', filename)[0])
     with open(json_file_name, 'w') as file:
-        # Incipit
-        file.write('Section: {}\n'.format('incipit'))
-        file.write('====================\n')
-        values = dictObj['incipit']
-        for v in values:
-            file.write('{}\n'.format(v))
-        file.write('\n')
+        section_names = dictObj['section_names']
 
-        # Numbered Sections
-        for k in sorted(dictObj.keys()):
-            if k in ['incipit', 'explicit']:
-                continue
-            file.write('Section: {}\n'.format(k))
+        for section in section_names:
+            file.write('Section: {}\n'.format(section))
             file.write('====================\n')
-            values = dictObj[k]
+            values = dictObj[section]
             for v in values:
                 file.write('{}\n'.format(v))
             file.write('\n')
 
-        # Explicit
-        file.write('Section: {}\n'.format('explicit'))
-        file.write('====================\n')
-        values = dictObj['explicit']
-        for v in values:
-            file.write('{}\n'.format(v))
-        file.write('\n')
     print('Created {}'.format(json_file_name))
 
 def readFile(filename):
@@ -66,12 +50,16 @@ def readFile(filename):
     lines = filter(None, lines) # remove empty strings from list
     return lines
 
-def readIntoDict(lines):
+def readIntoDict(lines, filename):
     d = defaultdict(list)
     section_found = False
     last_section_number = None
+    section_names = []
+    d['incipit'] = []
     for line in lines:
         if not (section_found or line.startswith('(')):
+            if 'incipit' not in section_names:
+                section_names.append('incipit')
             d['incipit'].append(line)
         elif line.startswith('('):
             # Match (X) at the beginning of a string where X is alpha-numeric string
@@ -80,23 +68,54 @@ def readIntoDict(lines):
                 last_section_number = str(int(last_section_number)).zfill(3)
             section_found = True
             d[last_section_number].append(line)
+            section_names.append(last_section_number)
         elif section_found:
             d[last_section_number].append(line)
             
     # inspect the last section for explicit (conclusion)
     d['explicit'] = []
+    section_names.append('explicit')
     last_section = d[last_section_number]
     copy = last_section[:]
     is_explicit = False
     if len(last_section) > 1:
         for line in last_section[1:]:
-            words_to_check = ["actum", "datum", "explicit", "expliciunt", "in cuius rei", "in cujus rei"]
+            words_to_check = getExplicitIdentifiers()
             if is_explicit or any(word in line.lower() for word in words_to_check):
                 is_explicit = True
                 d['explicit'].append(line)
                 copy.remove(line)
         d[last_section_number] = copy
-    return d 
+
+    # Update the dict if the file has no numbered section
+    if not section_found:
+        #print('{}'.format(filename))
+        new_dict = dict()
+        new_dict['text'] = lines
+        section_names = ['text']
+        d = new_dict
+
+    d['section_names'] = section_names
+    return d
+
+def getExplicitIdentifiers():
+    # if any of these identifiers appear in a line of the LAST numbered section,
+    # then that line belong in "explicit" section.
+    identifiers = ["actum", 
+    "datum", 
+    "explicit", 
+    "expliciunt", 
+    "in cuius rei", 
+    "in cujus rei", 
+    "conclusion", 
+    "escatocolo",
+    "se acaban",
+    "fecho en",
+    "leydas",
+    "dada en",
+    "diligencia notarial"]
+
+    return identifiers
 
 if __name__ == '__main__':
     main()
