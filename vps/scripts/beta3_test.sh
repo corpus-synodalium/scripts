e1435bb4005faed34c50cd03fdd99233c6dce4f0
thawsitt <thawsitt@gmail.com>
2021-06-28 16:32:18 -0700

    Remove access control (password)

--- vps/scripts/beta3_test.sh
+++ vps/scripts/beta3_test.sh
@@ -2,5 +2,5 @@ rm -rf ~/_output/*
 unzip ~/test.zip -d ~
 philoload4 -l ~/scripts/vps/config/custom_load_config.py beta3 ~/_output/*.xml
 cp -f ~/scripts/vps/config/web_config.cfg /var/www/html/philologic/beta3/data/web_config.cfg
-cp ~/scripts/vps/config/logins.txt /var/www/html/philologic/beta3/data/
+# cp ~/scripts/vps/config/logins.txt /var/www/html/philologic/beta3/data/
 echo "Don't forget to run fixpermission beta3"
--- vps/scripts/corpus.sh
+++ vps/scripts/corpus.sh
@@ -2,5 +2,5 @@ rm -rf ~/_output/*
 unzip ~/output.zip -d ~
 philoload4 -l ~/scripts/vps/config/custom_load_config.py corpus ~/_output/*.xml
 cp -f ~/scripts/vps/config/web_config.cfg /var/www/html/philologic/corpus/data/web_config.cfg
-cp ~/scripts/vps/config/logins.txt /var/www/html/philologic/corpus/data/
+# cp ~/scripts/vps/config/logins.txt /var/www/html/philologic/corpus/data/
 echo "Don't forget to run fixpermission corpus"
